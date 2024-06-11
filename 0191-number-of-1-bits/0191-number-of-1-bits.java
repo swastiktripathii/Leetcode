7a7b9f0281cc9e0ac4bab1d4f1b9bbe8ab0dfe14
Swastik Tripathi <91699294+swastiktripathii@users.noreply.github.com>
2024-06-11 14:21:43 +0530

    Time: 0 ms (100%), Space: 41.1 MB (6.55%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,20 @@
 class Solution {
     public int hammingWeight(int n) {
-        int cnt = 0;
-        while(n > 0){
-            n = n & (n - 1);
-            cnt++;
+        int count = 0, mask = 1;
+        for(int i = 0; i < 31; i++){
+            if((n&mask)!=0){
+                count++;
+            }
+            mask<<=1;
         }
-        return cnt;
+        return count;
+
+        //Bit Manipulation
+        // int cnt = 0;
+        // while(n > 0){
+        //     n = n & (n - 1);
+        //     cnt++;
+        // }
+        // return cnt;
     }
 }
