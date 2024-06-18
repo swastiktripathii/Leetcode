55604acea7a77c57065d5f6e5e8768c4e659d88f
Swastik Tripathi <91699294+swastiktripathii@users.noreply.github.com>
2024-06-18 14:17:37 +0530

    Time: 0 ms (100%), Space: 40.6 MB (13.27%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,6 +1,34 @@
+// Recursive Approach 
+
+// class Solution {
+//     public int fib(int n) {
+//         if(n<=1) return n;
+//         return fib(n-1) + fib(n-2);
+//     }
+// }
+
+// Top Down Memoization
+
 class Solution {
-    public int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1) + fib(n-2);
+    public static int[] dp = new int[31];
+    static {
+        Arrays.fill(dp, -1); 
+    }
+    public  int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int first, second;
+        if (dp[n - 1] != -1) {
+            first = dp[n - 1];
+        } else {
+            first = fib(n - 1);
+        }
+        if (dp[n - 2] != -1) {
+            second = dp[n - 2];
+        } else {
+            second = fib(n - 2);
+        }
+        return dp[n] = first + second;
     }
 }
