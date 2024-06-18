908a3e1f82c05273659905da19717c51e797fc4d
Swastik Tripathi <91699294+swastiktripathii@users.noreply.github.com>
2024-06-18 14:29:19 +0530

    Time: 0 ms (100%), Space: 40.8 MB (5.07%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -29,15 +29,36 @@
 //     }
 // }
 
+// Best Approach
+
+// class Solution{
+//     public int fib(int n){
+//         if(n == 0) return 0;
+//         int[] dp = new int[n+1];
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         for(int i = 2; i <= n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+// Optimized DP Approach
+
 class Solution{
-    public int fib(int n){
-        if(n == 0) return 0;
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+    public  int fib(int n) {
+        if (n <= 1) return n;
+        if(n == 2) return 1;
+        int curr = 0;
+        int prev1 = 1;
+        int prev2 = 1;
+
+        for(int i = 3; i <= n; i++){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
         }
-        return dp[n];
+        return curr;
     }
 }
