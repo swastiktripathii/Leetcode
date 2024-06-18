e1c5765ace2af6af53d86f202411cdb80885fc56
Swastik Tripathi <91699294+swastiktripathii@users.noreply.github.com>
2024-06-18 14:41:55 +0530

    Time: 0 ms (100%), Space: 40 MB (69.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=1) return 1;
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr = -1;
+        for(int i = 2;i<=n;i++){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return curr;
+    }
+}
