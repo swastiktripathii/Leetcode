9a5a301055cb94754c24208862a740b3bf2ad053
Swastik Tripathi <91699294+swastiktripathii@users.noreply.github.com>
2024-06-18 14:21:37 +0530

    Time: 0 ms (100%), Space: 40.4 MB (19.6%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -11,24 +11,20 @@
 
 class Solution {
     public static int[] dp = new int[31];
-    static {
-        Arrays.fill(dp, -1); 
-    }
     public  int fib(int n) {
         if (n <= 1) {
             return n;
         }
-        int first, second;
-        if (dp[n - 1] != -1) {
-            first = dp[n - 1];
-        } else {
-            first = fib(n - 1);
+        if(n == 2) return 1;
+        memoize(n);
+        return dp[n];
+    }
+    public int memoize(int N){
+        if(dp[N] != 0) return dp[N];
+        if(N < 3) return 1;
+        else{
+            dp[N] = memoize(N-1) + memoize(N-2);
         }
-        if (dp[n - 2] != -1) {
-            second = dp[n - 2];
-        } else {
-            second = fib(n - 2);
-        }
-        return dp[n] = first + second;
+        return memoize(N);
     }
 }
