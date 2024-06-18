a04cbe7efdc5954952904eb6319b2353afb0136c
Swastik Tripathi <91699294+swastiktripathii@users.noreply.github.com>
2024-06-18 14:26:07 +0530

    Time: 0 ms (100%), Space: 40.5 MB (13.27%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -9,22 +9,35 @@
 
 // Top Down Memoization
 
-class Solution {
-    public static int[] dp = new int[31];
-    public  int fib(int n) {
-        if (n <= 1) {
-            return n;
+// class Solution {
+//     public static int[] dp = new int[31];
+//     public  int fib(int n) {
+//         if (n <= 1) {
+//             return n;
+//         }
+//         if(n == 2) return 1;
+//         memoize(n);
+//         return dp[n];
+//     }
+//     public int memoize(int N){
+//         if(dp[N] != 0) return dp[N];
+//         if(N < 3) return 1;
+//         else{
+//             dp[N] = memoize(N-1) + memoize(N-2);
+//         }
+//         return memoize(N);
+//     }
+// }
+
+class Solution{
+    public int fib(int n){
+        if(n == 0) return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        if(n == 2) return 1;
-        memoize(n);
         return dp[n];
-    }
-    public int memoize(int N){
-        if(dp[N] != 0) return dp[N];
-        if(N < 3) return 1;
-        else{
-            dp[N] = memoize(N-1) + memoize(N-2);
-        }
-        return memoize(N);
     }
 }
