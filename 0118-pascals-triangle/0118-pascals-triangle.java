6c70671357e4b693eb59031ac0106f972884521c
Swastik Tripathi <91699294+swastiktripathii@users.noreply.github.com>
2024-07-08 14:25:15 +0530

    Time: 1 ms (82.67%), Space: 41.7 MB (65.34%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,26 +1,21 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> pascalTriangle = new ArrayList<>();
-
-        pascalTriangle.add(new ArrayList<>());
-        pascalTriangle.get(0).add(1);
-
-        for (int currentRow = 1; currentRow < numRows; currentRow++) {
-            pascalTriangle.add(new ArrayList<>());
-
-            List<Integer> currentRowList = pascalTriangle.get(currentRow);
-            List<Integer> previousRowList = pascalTriangle.get(currentRow - 1);
-
-            currentRowList.add(1);
-
-            for (int j = 1; j < previousRowList.size(); j++) {
-                int sum = previousRowList.get(j) + previousRowList.get(j - 1);
-                currentRowList.add(sum);
-            }
-
-            currentRowList.add(1);
+        List<List<Integer>> traingle = new ArrayList<>();
+        if(numRows == 0){
+            return new ArrayList();
         }
-
-        return pascalTriangle;
+            for(int i = 1; i<=numRows; i++){
+                List<Integer> row = new ArrayList();
+                for(int j = 0;j < i ; j++){
+                    if(j ==0|| j == i-1){
+                        row.add(1);
+                    }
+                    else{
+                        row.add(traingle.get(i-2).get(j)+traingle.get(i-2).get(j-1));
+                    }
+                }
+                traingle.add(row);
+            }
+        return traingle;
     }
 }
